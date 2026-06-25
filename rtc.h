140e795be940adafd3252be8964d10f8d3eda839
ganeshkolluru1803-lang <ganeshkolluru1803@gmail.com>
2026-06-25 13:42:33 +0530

    Update rtc.h

--- rtc.h
+++ rtc.h
@@ -3,22 +3,55 @@
 
 #include "types.h"
 
+/* Initialize the RTC module */
 void RTC_Init(void);
 
-void GetRTCTimeInfo(s32 *,s32 *,s32 *);
-void DisplayRTCTime(u32,u32,u32);
+/* Read current RTC time */
+void GetRTCTimeInfo(s32 *, s32 *, s32 *);
 
-void GetRTCDateInfo(s32 *,s32 *,s32 *);
-void DisplayRTCDate(u32,u32,u32);
+/* Display time on the LCD */
+void DisplayRTCTime(u32, u32, u32);
 
-void SetRTCTimeInfo(u32,u32,u32);
-void SetRTCDateInfo(u32,u32,u32);
+/* Read current RTC date */
+void GetRTCDateInfo(s32 *, s32 *, s32 *);
 
+/* Display date on the LCD */
+void DisplayRTCDate(u32, u32, u32);
+
+/* Set RTC time */
+void SetRTCTimeInfo(u32, u32, u32);
+
+/* Set RTC date */
+void SetRTCDateInfo(u32, u32, u32);
+
+/* Read current day of the week */
 void GetRTCDay(s32 *);
+
+/* Display day of the week */
 void DisplayRTCDay(u32);
+
+/* Set day of the week */
 void SetRTCDay(u32);
 
+/* Set hour */
 void sethour(u32 hour);
-void setminute(u32 minute);
-#endif
 
+/* Set minute */
+void setminute(u32 minute);
+
+/* Set second */
+void setsecond(u32 second);
+
+/* Set date */
+void setdate(u32 date);
+
+/* Set month */
+void setmonth(u32 month);
+
+/* Set year */
+void setyear(u32 year);
+
+/* Set day of the week */
+void setweek(u32 week);
+
+#endif
