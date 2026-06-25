6f880c51c42d2a939b0a2e9a7c4f7e08c9514057
ganeshkolluru1803-lang <ganeshkolluru1803@gmail.com>
2026-06-25 13:36:38 +0530

    Add comments to pin definitions in kpm_defines.h

--- kpm_defines.h
+++ kpm_defines.h
@@ -1,9 +1,9 @@
-#define ROW0 16
-#define ROW1 17
-#define ROW2 18
-#define ROW3 19
+#define ROW0 16    // Row 0 pin
+#define ROW1 17    // Row 1 pin
+#define ROW2 18    // Row 2 pin
+#define ROW3 19    // Row 3 pin
 
-#define COL0 20
-#define COL1 21
-#define COL2 22
-#define COL3 23
+#define COL0 20    // Column 0 pin
+#define COL1 21    // Column 1 pin
+#define COL2 22    // Column 2 pin
+#define COL3 23    // Column 3 pin
